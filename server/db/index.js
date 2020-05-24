a5c33ba73be4586ab2bb9fa409e471a3161985d4
NaseerHines <Hinesnaseer@gmail.com>
2020-05-24 16:48:00 -0500

    db stuff

---
+++ server/db/index.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const DB_HOST = "localhost";
+const DB_USER = "root";
+const DB_PASS = "";
+const DB_NAME = "siege";
+
+const connection = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME,
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Database connected");
+    }
+});
