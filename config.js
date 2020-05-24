1ba8d63286dc1162d8c33a00aababa5eb366df8e
NaseerHines <Hinesnaseer@gmail.com>
2020-05-24 16:45:42 -0500

    Moved some files around

--- config.js
+++
@@ -1,7 +0,0 @@
-const Hide = {
-  PORT: 8085,
-  NODE_ENV: "development",
-  TOKEN: "9daeb7ab-8cf3-4ac2-af17-08843554590c",
-  URL: "https://api2.r6stats.com/public-api/stats/",
-};
-module.exports.Hide = Hide;
