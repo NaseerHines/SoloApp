3709447e349621b0f101079b997cc78a9ba7055f
NaseerHines <Hinesnaseer@gmail.com>
2020-05-24 16:48:39 -0500

    navigations

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
-  <header>
+  <header class="header">
+    <h1>Siege Stat Tracker</h1>
     <img :src="image" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/Squads">Squad Search</router-link> |
+      <router-link to="/Frequently">Frequently Searched</router-link>
+    </div>
   </header>
 </template>
 <script>
@@ -8,14 +14,30 @@ export default {
   name: "Header",
   data() {
     return {
-      image: require("@/assets/logo.png")
+      image: require("@/assets/header.png")
     };
   }
 };
 </script>
 <style scoped>
+div#nav{
+  text-align: center;
+  color: black;
+  justify-content: center;
+  align-items: center;
+  max-width: 720px;
+  margin: 0 auto;
+  list-style: none;
+}
 img {
-  width: 300px;
+  width: 600px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+h1 {
+  text-align: center;
 }
 @media (max-width: 700px) {
   img {
