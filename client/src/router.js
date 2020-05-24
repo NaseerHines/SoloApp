dd4ed3e87cbbe63028a13a2d10408e0ca93b3768
NaseerHines <Hinesnaseer@gmail.com>
2020-05-24 16:47:43 -0500

    adjusted file requires

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import Router from "vue-router";
 import Search from "./components/Search.vue";
 import Profile from "./components/Profile.vue";
+import Frequently from "./components/Frequently.vue";
+import Squads from "./components/Squads.vue";
+import Team from "./components/Team.vue";
 Vue.use(Router);
 export default new Router({
   mode: "history",
@@ -15,6 +18,21 @@ export default new Router({
       path: "/profile/:platform/:gamertag",
       name: "profile",
       component: Profile,
+    },
+    {
+      path: "/Frequently",
+      name: "Frequently",
+      component: Frequently,
+    },
+    {
+      path: "/Squads",
+      name: "Squads",
+      component: Squads,
+    },
+    {
+      path: "/Team/:platform/:full1/:full2/:full3/:full4/:full5",
+      name: "Team",
+      component: Team,
     },
   ],
 });
