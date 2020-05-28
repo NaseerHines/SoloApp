6c9aa7a753f3616848cae375b7696989e4e6c833
NaseerHines <Hinesnaseer@gmail.com>
2020-05-28 12:08:44 -0500

    googleauth?

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueToasted from "vue-toasted";
+
 Vue.config.productionTip = false;
 // Load Toast Plugin
 Vue.use(VueToasted, {
